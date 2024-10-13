55d39fcb23bd5b0dfcf9fa5f2c03ebe7ead86d4d
kmozdzen <kmozdzen@onet.pl>
2024-10-13 20:16:18 +0200

    initial commit

---
+++ back/src/main/java/jnezdzom/gamedle/GamedleApplication.java
@@ -0,0 +1,13 @@
+package jnezdzom.gamedle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedleApplication.class, args);
+	}
+
+}
