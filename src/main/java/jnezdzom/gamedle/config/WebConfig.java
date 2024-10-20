22478b8f29d7dd253ca1c7a7cb3d260d84428847
kmozdzen <kmozdzen@onet.pl>
2024-10-20 17:32:15 +0200

    proxy2

---
+++ src/main/java/jnezdzom/gamedle/config/WebConfig.java
@@ -0,0 +1,17 @@
+package jnezdzom.gamedle.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://gamedle-khaki.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false);
+    }
+}
